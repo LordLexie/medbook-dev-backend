0339bdb681f7db4981dc8a758965e375aa931461
LordLexie <muigaifrancis@gmail.com>
2023-08-18 15:32:13 +0300

    Populated the create_patients_services_table migration file

--- database/migrations/2023_08_18_122225_create_patients_services_table.php
+++ database/migrations/2023_08_18_122225_create_patients_services_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('patients_services', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->unsignedBigInteger('patient');
+            $table->unsignedBigInteger('service');
+            $table->text('general_comments');
+
+            $table->foreign('patient')->references('id')->on('patients');
+            $table->foreign('service')->references('id')->on('services');
         });
     }
 
